6929149575c84acd0864445f0ea4d461757a2f32
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-03-25 19:48:44 +0600

    react router and font set up complete.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:     <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
